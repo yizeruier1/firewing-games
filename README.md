691edae7573cc9e5a9a94393370ef3881af13411
gostephen@163.com <gostephen@163.com>
2019-10-17 17:56:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# 基础
+### JavaScript
++ JS调用堆栈
++ Promise (async/await)
++ 原型继承和原型链
+
+### 其他
++ TypeScript 高阶类型
+
+### 开发应用
++ Vue 生命周期
++ Vue 的实现原理
++ Vue 父子组件交互
++ CSS 规范
+
+### Vue 实现 TodoList
---
+++ todoList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        asdasd
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'todoList',
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="css">
+    
+</style>
